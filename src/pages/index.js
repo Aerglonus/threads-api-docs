69740cd5769c6c479fe1096e877150c2aecfe4c5
lonus <calebcontreras8@gmail.com>
2023-07-23 00:09:41 -0700

    fix: button-icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './index.module.css';
 
 function HomepageHeader() {
@@ -22,7 +21,7 @@ function HomepageHeader() {
             to="/docs/basics/Getting-Started">
 
             <div className='button-text'>
-              <FontAwesomeIcon icon="fa-solid fa-book" />
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z" /></svg>
               <p>Documentation</p>
             </div>
           </Link>
