9211aa94c09b34a1d1d353463ef703f9f66a2af5
lonus <calebcontreras8@gmail.com>
2023-07-22 23:42:38 -0700

    smh

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/current/basics/Getting-Started">
+            to="/docs/basics/Getting-Started">
 
             <div className='button-text'>
               <FontAwesomeIcon icon="fa-solid fa-book" />
